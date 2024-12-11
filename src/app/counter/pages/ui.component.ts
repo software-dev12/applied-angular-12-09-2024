b49729aac1a9e81071481cff5ec192df96a17629
Anthony Nero <software_developer12@outlook.com>
2024-12-11 10:41:22 -0500

    code challenge 2 - fizz zuzz

--- src/app/counter/pages/ui.component.ts
+++ src/app/counter/pages/ui.component.ts
@@ -21,19 +21,34 @@ import {
       <span data-testid="current">{{ current() }}</span>
       <button (click)="increment()" class="btn btn-primary">+</button>
     </div>
+    <div>
+      <p>{{ fizzBuzz() }}</p>
+    </div>
   `,
   styles: ``,
 })
 export class UiComponent {
   current = signal(0);
+  textToDisplay = signal('');
 
   increment() {
     this.current.update((r) => r + 1);
+    this.fizzBuzz();
   }
 
   decrement() {
     this.current.update((r) => r - 1);
+    this.fizzBuzz();
   }
 
   decrementDisabled = computed(() => this.current() === 0);
+
+  fizzBuzz = computed(() => {
+    const current = this.current();
+    if (current === 0) return '';
+    if (current % 3 === 0 && current % 5 === 0) return 'FizzBuzz';
+    if (current % 3 === 0) return 'Fizz';
+    if (current % 5 === 0) return 'Buzz';
+    return '';
+  });
 }
